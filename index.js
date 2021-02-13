017c4da4ca53c1f8a68d5f7896ee175977be5458
Sanchit <sanchitrrk@gmail.com>
2021-02-13 16:52:21 +0530

    read env

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const NAME = process.env.NAME || 'undefined';
+
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
+  response.send(`<h1>Hello ${NAME}</h1>`);
 });
 
 const PORT = process.env.PORT || 8080;
