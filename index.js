1594d4f724978a3c7069b8471e84c1dc343d2335
Sanchit <sanchitrrk@gmail.com>
2021-02-13 17:24:07 +0530

    trying...

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 
 const NAME = process.env.NAME || 'undefined';
+const AGE = process.env.AGE || 'undefined';
 
 app.get('/', (request, response) => {
-  response.send(`<h1>Hello ${NAME}</h1>`);
+  response.send(`<h1>Hello ${NAME} ${AGE}</h1>`);
 });
 
 const PORT = process.env.PORT || 8080;
